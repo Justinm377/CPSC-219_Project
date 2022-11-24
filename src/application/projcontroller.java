bd4a424cf60d4acbcd43a87ef4e4292ea74b3af0
KhushiChoksi <CS219-user@10.0.0.173>
2022-11-24 11:33:37 -0700

    Added code that generates a random order number, for final scene.

--- src/application/projcontroller.java
+++ src/application/projcontroller.java
@@ -11,13 +11,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.util.*;
+
 public class projcontroller {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
 	@FXML
-    private Label orderNumber;
+    public Label orderNumber;
 	
 	public void switchtoHomePage(ActionEvent event) throws IOException {
 		root=FXMLLoader.load(getClass().getResource("HomePage.fxml"));
@@ -55,11 +57,21 @@ public class projcontroller {
 		stage.setScene(scene);
 		stage.show();
 	}
-	public void switchtoOrderComfirmation(ActionEvent event) throws IOException {
+	public void switchtoOrderComfirmation(ActionEvent event) throws IOException {	
 		Parent root=FXMLLoader.load(getClass().getResource("Order_Confirmation.fxml"));
 		stage=(Stage)((Node)event.getSource()).getScene().getWindow();
 		scene= new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+		
+		//generate random order number
+		//this site was used to learn about the random class, and how it works: https://www.digitalocean.com/community/tutorials/java-random
+		String orderNumberAsString = ""; 
+		Random randomNumbers = new Random();
+		int number = 0;
+		for (int i = 0; i < 6; i++) {
+			number = randomNumbers.nextInt(10);
+			orderNumberAsString = orderNumberAsString + Integer.toString(number);
+		}
 	}
 }

a8595ba147ac1fa6470930c6ed1a76c2a4965db3
KhushiChoksi <CS219-user@10.0.0.173>
2022-11-25 23:17:36 -0700

    Created constructor and calculateTotalPrice method in TotalPrice. TotalPrice is child of MenuItem.

--- src/application/TotalPrice.java
+++ src/application/TotalPrice.java
@@ -1,9 +1,22 @@
 package application;
 
-public class TotalPrice {
+import java.util.ArrayList;
 
-	public TotalPrice() {
-		// TODO Auto-generated constructor stub
+public class TotalPrice extends MenuItem {
+	private ArrayList<MenuItem> itemList; 
+	
+	public TotalPrice(ArrayList<MenuItem> foodItemList) {
+		itemList = foodItemList;
+	}
+	
+	public double calculateTotalPrice() {
+		double priceToReturn = 0.00;
+		
+		for (int i = 0; i < itemList.size(); i++) {
+			priceToReturn = priceToReturn + itemList.get(i).calculateItemTotalPrice();
+		}
+		
+		return priceToReturn;
 	}
 
 }
--- src/application/projcontroller.java
+++ src/application/projcontroller.java
@@ -18,47 +18,53 @@ import java.util.*;
 
 
 public class projcontroller {
-	private double priceSamosa = 5.99;
-	private double pricePaniPuri = 8.99;
-	private double priceButterChicken = 17.99;
-	private double priceChefsChoice = 14.99;
-	private double priceGulabJamun = 6.99;
-	private double priceBrownie = 4.99;
-	private double totalPrice = 0.0;
 
-	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
 	@FXML
-    private Label orderNumber;	
+    private Label orderNumber;
+	
 	@FXML
     private TextField samosaTextField;
+	
     @FXML
-    private CheckBox samosaCheckBox;   
+    private CheckBox samosaCheckBox; 
+    
     @FXML
     private TextField paniPuriTextField;
+    
     @FXML
-    private CheckBox paniPuriCheckBox;   
+    private CheckBox paniPuriCheckBox; 
+    
     @FXML
     private TextField butterChickenTextField;
+    
     @FXML
-    private CheckBox butterChickenCheckBox;    
+    private CheckBox butterChickenCheckBox;
+    
     @FXML
     private TextField chefsChoiceTextField;
+    
     @FXML
-    private CheckBox chefsChoiceCheckBox;   
+    private CheckBox chefsChoiceCheckBox;
+    
     @FXML
     private TextField gulabJamunTextField;
+    
     @FXML
-    private CheckBox gulabJamunCheckBox;   
+    private CheckBox gulabJamunCheckBox;
+    
     @FXML
     private TextField brownieTextField;
+    
     @FXML
-    private CheckBox brownieCheckBox;    
+    private CheckBox brownieCheckBox;  
+    
     @FXML
     private ChoiceBox <String> paymentTypeChoiceBox;
+    
     @FXML
     private Label totalPriceLabel;
     
@@ -78,20 +84,6 @@ public class projcontroller {
     		
     	}
     }
-    
-    private double ifSelected(CheckBox foodChoiceBox, TextField quantityValue, Double price) {
-  	  double quantityTotal = 0;
-  	  if (foodChoiceBox.isSelected() && quantityValue.getText() != null) {  	  
-	  	  //if ( argument  != null) {
-	  		 String value = quantityValue.getText();
-	  		 double valueNumber = Double.parseDouble(value);
-	  		  quantityTotal = valueNumber*price;
-	  	  //}
-  	  }else {
-  		  quantityTotal = 0;
-  	  }
-  		 return quantityTotal;
-     }
     
     @FXML
     void samosaTextFieldAppears(ActionEvent event) {
@@ -122,21 +114,6 @@ public class projcontroller {
     void brownieTextFieldAppears(ActionEvent event) {
        	setTextFieldVisibility(brownieCheckBox,brownieTextField );
     }
-	
-    public void  getTotalPrice() {
-    	double totalSamosa = ifSelected(samosaCheckBox,samosaTextField, priceSamosa);
-    	double totalPaniPuri = ifSelected(paniPuriCheckBox,paniPuriTextField, pricePaniPuri);
-    	double totalButterChicken = ifSelected(butterChickenCheckBox,butterChickenTextField, priceButterChicken);
-    	double totalChefsChoice = ifSelected(chefsChoiceCheckBox,chefsChoiceTextField, priceChefsChoice);
-    	double totalGulabJamun = ifSelected(gulabJamunCheckBox,gulabJamunTextField, priceGulabJamun);
-    	double totalBrownie = ifSelected(brownieCheckBox,brownieTextField, priceBrownie);
-    	
-    	
-    	totalPrice = totalSamosa + totalPaniPuri + totalButterChicken + totalChefsChoice + totalGulabJamun + totalBrownie;
-    	String totalPriceStr = Double.toString(totalPrice);
-    	
-    	totalPriceLabel.setText(totalPriceStr);
-    }
 
 	public void switchtoHomePage(ActionEvent event) throws IOException {
 		root=FXMLLoader.load(getClass().getResource("HomePage.fxml"));
@@ -156,21 +133,53 @@ public class projcontroller {
 		stage.show();
 	}
 	public void switchtoOrderMenu(ActionEvent event) throws IOException {
-		root=FXMLLoader.load(getClass().getResource("Order_Menu.fxml"));
+		FXMLLoader loader = new FXMLLoader();
+		Parent root = loader.load(new FileInputStream("src/application/Order_Menu.fxml"));
+		projcontroller controller = (projcontroller)(loader.getController());
 		stage=(Stage)((Node)event.getSource()).getScene().getWindow();
 		scene= new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+		
+		/*double priceSamosa = 5.99;
+		double pricePaniPuri = 8.99;
+		double priceButterChicken = 17.99;
+		double priceChefsChoice = 14.99;
+		double priceGulabJamun = 6.99;
+		double priceBrownie = 4.99;
+		double totalPrice = 0.0;
+		
+		try {
+			MenuItem samosa = new MenuItem(controller.samosaTextField.getText(), priceSamosa);
+			MenuItem paniPuri = new MenuItem(controller.paniPuriTextField.getText(), pricePaniPuri);
+			MenuItem butterChicken = new MenuItem(controller.butterChickenTextField.getText(), priceButterChicken);
+			MenuItem chefsChoice = new MenuItem(controller.chefsChoiceTextField.getText(), priceChefsChoice);
+			MenuItem gulabJamun = new MenuItem(controller.gulabJamunTextField.getText(), priceGulabJamun);
+			MenuItem brownie = new MenuItem(controller.brownieTextField.getText(), priceBrownie);
+			
+			totalPrice = samosa.calculateItemTotalPrice() + paniPuri.calculateItemTotalPrice() + 
+					butterChicken.calculateItemTotalPrice() + chefsChoice.calculateItemTotalPrice() +
+					gulabJamun.calculateItemTotalPrice() + brownie.calculateItemTotalPrice();
+		} catch (InvalidUserInputException e) {
+			e.printStackTrace();
+		} 
+		//NOTE: this code is not working right now because of the Controller issue. Controller needs to be separated into multiple */
+		
 	}
 	public void switchtoPaymentSummary(ActionEvent event) throws IOException {
-		root=FXMLLoader.load(getClass().getResource("Payment Summary.fxml"));
+		FXMLLoader loader = new FXMLLoader();
+		Parent root = loader.load(new FileInputStream("src/application/Payment Summary.fxml"));
+		projcontroller controller = (projcontroller)(loader.getController());
 		stage=(Stage)((Node)event.getSource()).getScene().getWindow();
 		scene= new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
-		//getTotalPrice();
+		
+//		String totalPriceAsString = Double.toString(totalPrice); These two lines of code will will not work right now. This is because the commented code in above method does not work. 
+//		controller.totalPriceLabel.setText("hello"); 
+		
 	}
 	public void switchtoPaymentInfo(ActionEvent event) throws IOException {
 		root=FXMLLoader.load(getClass().getResource("Payment Info.fxml"));

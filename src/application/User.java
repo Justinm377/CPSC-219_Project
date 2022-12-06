53389ef09afeb55baa88159c244aafd18eb1e068
KhushiChoksi <CS219-user@10.0.0.173>
2022-12-06 13:37:08 -0700

    Added javadoc and commenting to User class.

--- src/application/User.java
+++ src/application/User.java
@@ -1,29 +1,45 @@
 package application;
 
+/**
+ * Stores information about an user, such as their name, address, postal code, and phone number. This information can
+ * be set based on valid user input, and later accessed. 
+ * @author CS219-user Group 5
+ *
+ */
 public class User {
-
 	private String firstName; 
 	private String lastName; 
 	private String address;
 	private String postalCode;
 	private String phoneNumber;
 
+	/**
+	 * Initializes a new User. 
+	 */
 	public User() {
 	}
 
+	/**
+	 * @return the first name of the user
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
-
+	
+	/**
+	 * Sets the user's input of their first name, if input is valid. Valid input contains only letter characters. 
+	 * @param firstName A string intended to be the first name of the user. 
+	 * @throws InvalidUserInputException if the user inputs characters apart from letters in first name or 
+	 * does not input anything. 
+	 */
 	public void setFirstName(String firstName) throws InvalidUserInputException {
-
 		//checks if first name input given by user is only letters, and if not, throws invalid input exception
 		if (firstName == "") {
-			throw new InvalidUserInputException("Please input your first name in this field.");
+			throw new InvalidUserInputException("Please input your first name in this field."); //user doesn't input anything in text field, so exception is thrown
 		} else if (firstName != "") {	
 			for (char c : firstName.toCharArray()){
 				if (!Character.isLetter(c)&& c!=' ') {
-					throw new InvalidUserInputException("Only include letters in this field.");
+					throw new InvalidUserInputException("Only include letters in this field."); //user inputs numbers in text field, so exception is thrown
 				}
 			}
 		} else {
@@ -31,20 +47,28 @@ public class User {
 		}
 	}
 
-
+	/**
+	 * @return the last name of the user
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * Sets the user's input of their last name, if input is valid. Valid input contains only letter characters.
+	 * @param lastName A string intended to be the last name of the user.
+	 * @throws InvalidUserInputException if the user inputs characters apart from letters in last name or does not 
+	 * input anything. 
+	 */
 	public void setLastName(String lastName) throws InvalidUserInputException {
 
 		//checks if last name input given by user is only letters, and if not, throws invalid input exception
 		if (lastName == "") {
-			throw new InvalidUserInputException("Please input your last name in this field.");
+			throw new InvalidUserInputException("Please input your last name in this field."); //user doesn't input anything in text field, so exception is thrown
 		} else if (lastName != "") {	
 			for (char c : lastName.toCharArray()){
 				if (!Character.isLetter(c) && c!=' ') {
-					throw new InvalidUserInputException("Only include letters in this field.");
+					throw new InvalidUserInputException("Only include letters in this field."); //user inputs characters other than letters, so exception is thrown
 				}
 			}
 		} else {
@@ -52,14 +76,19 @@ public class User {
 		}
 	}
 
+	/**
+	 * @return the address of the user 
+	 */
 	public String getAddress() {
 		return address;
 	}
 
 	/**
-	 * 
-	 * @param address
-	 * @throws InvalidUserInputException 
+	 * Sets the user's input of their address, if input is valid. Valid input contains only letter or number characters. 
+	 * and one hash is allowed. 
+	 * @param address A string intended to be the address of the user. 
+	 * @throws InvalidUserInputException if the user inputs characters apart from letters or numbers, more than one 
+	 * hash character, or does not input anything. 
 	 */
 	public void setAddress(String address) throws InvalidUserInputException {
 		int hashCount = 0; //user is allowed to input one hash (some addresses contain a hash)
@@ -76,7 +105,7 @@ public class User {
 					throw new InvalidUserInputException("Only one # is allowed to be used in this field.");
 				}
 				if (!Character.isDigit(c) && !Character.isLetter(c) && c!=' ' && c != '#' && (hashCount == 0 || hashCount == 1)) {
-					throw new InvalidUserInputException("Only include letters or numbers in this field.");
+					throw new InvalidUserInputException("Only include letters or numbers in this field."); //user enters an invalid character (apart from a hash), so exception is thrown
 				}
 			}
 		} else {
@@ -85,28 +114,31 @@ public class User {
 	}
 
 	/**
-	 * @return the postalCode
+	 * @return the postal code of the user
 	 */
 	public String getPostalCode() {
 		return postalCode;
 	}
 
 	/**
-	 * @param postalCode the postalCode to set
-	 * @throws InvalidUserInputException 
+	 * Sets the user's input of their postal code, if input is valid. Valid input is in the format "X1X1X1" and
+	 * contains only 6 characters (only letters and numbers, no white spaces). 
+	 * @param postalCode A string intended to be the postal code of the user. 
+	 * @throws InvalidUserInputException if the user doesn't input a postal code with format "X1X1X1", input has more 
+	 * than 6 characters, input has characters other than letters or numbers, or user doesn't input anything 
 	 */
 	public void setPostalCode(String postalCode) throws InvalidUserInputException {
 		if (postalCode == "") {
 			throw new InvalidUserInputException("Please input your postal code in this field.");
 		} else if (postalCode != "") {	
 			for (char c : postalCode.toCharArray()){
-				if (c == ' ') {
+				if (c == ' ') { //user may have entered postal code in format "X1X 1X1", so an exception is thrown
 					throw new InvalidUserInputException("Postal code should be in this format: X1X1X1");
 				}
-				if (postalCode.replace(" ", "").length() != 6 && c != ' ' && (Character.isDigit(c) || Character.isLetter(c))) {
+				if (postalCode.replace(" ", "").length() != 6 && c != ' ' && (Character.isDigit(c) || Character.isLetter(c))) { //user cannot input more or less than 6 characters 
 					throw new InvalidUserInputException (String.format("Postal should have 6 characters, not %d characters.", postalCode.length()));
 				}
-				if (!Character.isDigit(c) && !Character.isLetter(c)) {
+				if (!Character.isDigit(c) && !Character.isLetter(c)) { //user can only input letters or numbers
 					throw new InvalidUserInputException("Only include letters or numbers in this field, no whitespaces as well.");
 				}
 			}
@@ -121,32 +153,37 @@ public class User {
 		}	
 	}
 
+	/**
+	 * @return the phone number of the user
+	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
 	/**
-	 * @param phoneNumber the phoneNumber to set
-	 * @throws InvalidUserInputException 
+	 * Sets the user's input of their phone number, if input is valid. Valid input contains 10 digits, and characters
+	 * that are only numbers. 
+	 * @param phoneNumber A string intended to be the phone number of the user. 
+	 * @throws InvalidUserInputException if the user inputs an phone number containing characters other than numbers,
+	 * more than or less than 10 digits, or inputs nothing. 
 	 */
 	public void setPhoneNumber(String phoneNumber) throws InvalidUserInputException {
 
 		//checks if phone number input given by user has no letters and is 10 digits long
 		if (phoneNumber == "") {
-			throw new InvalidUserInputException("Please input your phone number in this field.");
+			throw new InvalidUserInputException("Please input your phone number in this field."); //user doesn't enter anything so exception is thrown
 		} else if (phoneNumber != "") {
 			for (char c : phoneNumber.toCharArray()){
-				if (phoneNumber.length() != 10 && Character.isDigit(c)) {
+				if (phoneNumber.length() != 10 && Character.isDigit(c)) { //user cannot enter more than or less than 10 digits 
 					throw new InvalidUserInputException (String.format("Phone number should have 10 digits, not %d digits.", phoneNumber.length()));
 				}
-				if (!Character.isDigit(c) && c!=' ') {
+				if (!Character.isDigit(c) && c!=' ') { //user cannot enter non-digit characters 
 					throw new InvalidUserInputException("Only include numbers in this field.");
 				}
 			}
 		} else {
 			this.phoneNumber = phoneNumber; //sets the instance variable when valid input is given
 		}
-
 	}
 
 }

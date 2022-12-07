e601e78aa72fccd8f3afdc81e0025c4163c28291
KhushiChoksi <CS219-user@10.0.0.173>
2022-12-06 21:31:13 -0700

    Updated some variable names.

--- src/application/MenuItem.java
+++ src/application/MenuItem.java
@@ -20,12 +20,12 @@ public class MenuItem {
 	/**
 	 * This constructor takes the price of item and the user input for quantity of item and sets the instance variables,
 	 * provided that the user inputs a valid positive integer value between 0 and 10 for quantity. 
-	 * @param amountItemAsString a String that holds the value of the amount of item intended by the user to be a quantity of that item. 
+	 * @param quantityItemAsString a String that holds the value of the amount of item intended by the user to be a quantity of that item. 
 	 * @param priceItem a double that holds the price of the item.
 	 * @throws InvalidUserInputException is thrown when user input for amount of item is anything other than a positive integer between 0 and 10. 
 	 */
-	public MenuItem(String amountItemAsString, Double priceItem) throws InvalidUserInputException {
-		setQuantity(amountItemAsString);
+	public MenuItem(String quantityItemAsString, Double priceItem) throws InvalidUserInputException {
+		setQuantity(quantityItemAsString);
 		setPrice(priceItem);
 	}
 
@@ -40,42 +40,42 @@ public class MenuItem {
 
 	/**
 	 * This setter method is a mutator method that sets the quantity instance variable, as long as the quantity
-	 * is a positive integer between 0 and 10. 
-	 * @param amountItemAsString a String intended to be a quantity value entered by the user. 
+	 * is a positive integer between 1 and 10. 
+	 * @param quantityItemAsString a String intended to be a quantity value entered by the user. 
 	 * @throws InvalidUserInputException is thrown when quantity of item is anything other than a positive integer between 1 and 10.
 	 */
-	public void setQuantity(String amountItemAsString) throws InvalidUserInputException {
-		int amountItem = 0;
+	public void setQuantity(String quantityItemAsString) throws InvalidUserInputException {
+		int quantityItem = 0;
 
 		int decimalCounter = 0;
-		boolean validAmount = true;
-		if (amountItemAsString != "" || amountItemAsString != null) {
-			for (char c : amountItemAsString.toCharArray()) { //if anything other than an integer number is provided, it's not valid
-				if (!Character.isDigit(c)) validAmount = false;
+		boolean validQuantity = true;
+		if (quantityItemAsString != "" || quantityItemAsString != null) {
+			for (char c : quantityItemAsString.toCharArray()) { //if anything other than an integer number is provided, it's not valid
+				if (!Character.isDigit(c)) validQuantity = false;
 				if (c == '.') decimalCounter++;
-				if (c == '0' && amountItemAsString.length() == 1) throw new InvalidUserInputException("Order quantity cannot be 0."); //user cannot order 0 items
+				if (c == '0' && quantityItemAsString.length() == 1) throw new InvalidUserInputException("Order quantity cannot be 0."); //user cannot order 0 items
 			}
-		} else if (amountItemAsString == "" || amountItemAsString == null) { //checks for when an user enters no input
-			validAmount = false;
-			amountItem = 0;
+		} else if (quantityItemAsString == "" || quantityItemAsString == null) { //checks for when an user enters no input
+			validQuantity = false;
+			quantityItem = 0;
 			throw new InvalidUserInputException("Please input a value in this field.");
 		}
 
 		//non-numeric value for the amount will throw an exception
-		if (validAmount == true && amountItemAsString != "") {
-			amountItem = Integer.parseInt(amountItemAsString);
-		} else if (validAmount == false && decimalCounter != 0) {
+		if (validQuantity == true && quantityItemAsString != "") {
+			quantityItem = Integer.parseInt(quantityItemAsString);
+		} else if (validQuantity == false && decimalCounter != 0) {
 			throw new InvalidUserInputException("Order quantity cannot contain decimals.");
-		} else if (validAmount == false && decimalCounter == 0) {
+		} else if (validQuantity == false && decimalCounter == 0) {
 			throw new InvalidUserInputException("Order quantity cannot contain non-numeric characters. Please enter a positive numeric value.");
 		}
 
 		//let's assume that the max amount an user can order of a quantity is 10
-		if (amountItem > 10) {
+		if (quantityItem > 10) {
 			throw new InvalidUserInputException("To order a quantity of more than 10, please contact us directly by phone or in-person.");
 		}
 
-		this.quantity = amountItem;
+		this.quantity = quantityItem;
 	}
 
 	/**

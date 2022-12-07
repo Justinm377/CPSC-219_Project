2116a2f3fd85113367ad6a8da44748d3775a4324
KhushiChoksi <CS219-user@10.0.0.173>
2022-12-07 01:13:01 -0700

    Added Javadoc to the OrderConfirmationController.

--- src/application/OrderConfirmationController.java
+++ src/application/OrderConfirmationController.java
@@ -11,6 +11,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+/**
+ * The OrderConfirmationController switches scenes from the order confirmation page to the home page. The order number is set when
+ * the order confirmation page is opened. 
+ * @author CS219-user Group 5
+ *
+ */
 public class OrderConfirmationController {
 
 	private Stage primaryStage;
@@ -21,27 +27,49 @@ public class OrderConfirmationController {
 	@FXML
 	private Label orderNumber;
 
+	/**
+	 * Sets the given stage as the primary stage of the application. 
+	 * @param aStage The stage to be set as the application's primary stage.
+	 */
 	public void setPrimaryStage(Stage aStage) {
 		primaryStage = aStage;
 	}
 
+	/**
+	 * Sets the given scene as the controller's scene.
+	 * @param aScene The scene intended to be displayed to the user.
+	 */
 	public void setMyScene(Scene aScene) {
 		myScene = aScene;
 	}
 
+	/**
+	 * Switches the controller from the PaymentInfoController to the OrderConfirmationController. 
+	 * @param next The next controller. 
+	 */
 	public void setNextController(PaymentInfoController next) {
 		paymentInfoController = next;
 	}
 
+	/**
+	 * Sets the given scene as the primary stage's scene. This displays the given scene to the user utilizing the application.
+	 */
 	public void takeFocus() {
 		primaryStage.setScene(myScene);
 	}
 	
-	public void setLabelText(String randomNum) {
-		String num = randomNum;
-		orderNumber.setText(num); //sets the order number label to randomly generated number
+	/**
+	 * Sets the order number label as the randomly generated number.
+	 * @param randomNumber The random number to set as the order number. 
+	 */
+	public void setLabelText(String randomNumber) {
+		orderNumber.setText(randomNumber); 
 	}
 
+	/**
+	 * Switches scenes from the order confirmation page to the home page, when user decides to go to the home page.
+	 * @param event Represents the action of the user pressing the button to return to the home page. 
+	 */
 	@FXML
 	public void switchToHomePage(ActionEvent event) { 
 		if (homePageController == null) {
